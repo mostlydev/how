e1a2205b450d0454a6cf042c08d1927bfa90754a
Wojtek <wojtek@grabski.ca>
2025-01-15 18:28:28 -0500

    chore: bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="howdoi-cli",
-    version="0.1.0",
+    version="0.2.0",
     author="Wojtek Grabski",
     author_email="mostlydev@mostlydev.com",
     description="AI-powered CLI tool for shell commands",
